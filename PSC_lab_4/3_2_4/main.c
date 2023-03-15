f18b139cb4a6bd1b567428ffdd519e342316bcc2
dfh <115549868+alinaupyrova@users.noreply.github.com>
2023-03-15 11:42:06 +0100

    add

--- PSC_lab_4/3_2_4/main.c
+++ PSC_lab_4/3_2_4/main.c
@@ -10,7 +10,7 @@ void change(int *x, int *y){
 }
 int main()
 {
-   int x = 4,y = 8;
+   int x = 4,y = 2;
    printf("x = %d, y = %d\n",x,y);
    change(&x,&y);
      printf("x = %d, y = %d\n",x,y);
