634d44c1c36d12c912d0a4b89c88de56b63b7208
dfh <115549868+alinaupyrova@users.noreply.github.com>
2023-03-15 11:41:45 +0100

    add

---
+++ PSC_lab_4/3_2_4/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void change(int *x, int *y){
+    int temp = *x;
+    if( *y < *x){
+        *x = *y;
+        *y = temp;
+    }
+}
+int main()
+{
+   int x = 4,y = 8;
+   printf("x = %d, y = %d\n",x,y);
+   change(&x,&y);
+     printf("x = %d, y = %d\n",x,y);
+    return 0;
+}
