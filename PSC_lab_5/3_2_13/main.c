3b4be0594a30d25322f348ed5858b1f6da23aebd
dfh <pakoszborys@gmail.com>
2023-03-22 12:04:36 +0100

    add

---
+++ PSC_lab_5/3_2_13/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+double foo(double(*wsk)(int), int x)
+{
+    return wsk(x);
+}
+double funkcja_pom(int arg)
+{
+    return (double)(arg+9);
+}
+int main()
+{
+  printf("%lf\n", foo(funkcja_pom,3));
+  return 0;
+}
