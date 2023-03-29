c74bc686049bdb490a326b080584c57423542bf4
alinaupyrova <a.upyrova2005@gmail.com>
2023-03-29 12:48:12 +0200

    add

--- PSC_lab_6/4_2_7/main.c
+++ PSC_lab_6/4_2_7/main.c
@@ -10,10 +10,8 @@ int foo7(int n, int tab1[], int tab2[], int tab3[])
         sum = tab1[i] + tab2[i];
         tab3[i] = sum;
     }
-
 }
-
-void print_array3(int n, int tab3[])
+void print_tab3(int n, int tab3[])
 {
     for(int i=0;i<n;i++)
     {
@@ -26,6 +24,6 @@ int main()
     int tab2[] = {2,5,6,-1};
     int tab3[] = {};
     foo7(4, tab1, tab2, tab3);
-    print_array3(4,tab3);
+    print_tab3(4,tab3);
     return 0;
 }
---
+++ PSC_lab_6/4_2_7b/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int foo7(int n, int tab1[], int tab2[], int tab3[])
+{
+   int temp = 0;
+   for( int i = 0; i < n; i++)
+   {
+       if ( tab1[i] > tab2[i])
+       {
+           temp = tab1[i];
+           tab3[i] = temp;
+       }
+       if (tab2[i] > tab1[i])
+        {
+            temp = tab2[i];
+            tab3[i] = temp;
+        }
+   }
+}
+void print_tab3(int n, int tab3[])
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("[%d]=%d\n",i, tab3[i]);
+    }
+}
+int main()
+{
+    int tab1[] = {1,-5,0,4};
+    int tab2[] = {2,-8,6,1};
+    int tab3[] = {};
+    foo7(4, tab1, tab2, tab3);
+    print_tab3(4,tab3);
+    return 0;
+}
