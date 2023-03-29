3f93c05f1989e4a4646d1e09585152536c96da43
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-03-29 10:59:00 +0200

    Update main.c

--- PSC_lab_5/zad_3/main.c
+++ PSC_lab_5/zad_3/main.c
@@ -3,7 +3,7 @@
 
 int foo(int n)
 {
-    if(n==0 || n ==1)
+    if(n==0 || n==1)
     {
         return 1;
     }
@@ -13,18 +13,18 @@ int foo(int n)
     }
     if(n % 3 == 1)
     {
-         return foo((n-1)/3+1);
-         return foo(n-1)-1;
+         return foo(n-1/3)+1;
+    }
+    return foo(n-1)-1;
     }
 
-}
 int main()
 {
    printf("%d\n", foo(0));
    printf("%d\n", foo(1));
-   printf("%d\n", foo(2));
-   printf("%d\n", foo(3));
    printf("%d\n", foo(4));
-   printf("%d\n", foo(5));
-    return 0;
+   printf("%d\n", foo(3));
+   printf("%d\n", foo(8));
+   printf("%d\n", foo(10));
+   return 0;
 }
