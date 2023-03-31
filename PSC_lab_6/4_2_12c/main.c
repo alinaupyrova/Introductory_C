84e9ab2d2d7f056de266e7f33c35d4136faf7927
alinaupyrova <a.upyrova2005@gmail.com>
2023-03-31 20:25:59 +0200

    add

---
+++ PSC_lab_6/4_2_12c/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void function(int n, int *tab)
+{
+    int temp = tab[0];
+    for(int i = 0; i < n - 1; i++)
+    {
+      tab[i+1] = tab[i];
+      tab[0] = temp;
+    }
+}
+int main()
+{
+    int n = 4;
+    int tab[] = {1,5,3,0};
+    function(n, tab);
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d\n", *(tab+i));
+    }
+    return 0;
+}
