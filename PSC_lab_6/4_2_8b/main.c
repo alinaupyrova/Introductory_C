d690ea7305781b65dbec2610612f5f5c05603665
alinaupyrova <a.upyrova2005@gmail.com>
2023-03-31 18:26:51 +0200

    add

---
+++ PSC_lab_6/4_2_8b/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void function(int n, int *tab1, int *tab2, double *tab3)
+{
+  for(int i = 0; i < n; i++)
+  {
+      if (i % 2 != 0)
+        {
+           *(tab3 + i) = (double)*(tab1 + i);
+        }
+  }
+   for (int i = 0; i < n; i++)
+    {
+
+        if ( i % 2 == 0)
+        {
+          *(tab3 + n + i) = (double)*(tab2 + i);
+        }
+    }
+}
+int main()
+{   int n = 3;
+    int tab1[] = {1,2,3};
+    int tab2[] = {2,5,8};
+    double tab3[2*n];
+    function(n, tab1, tab2, tab3);
+    for(int i = 0; i < 2 * n; i++)
+    {
+        printf("%lf\n", *(tab3+i));
+    }
+    return 0;
+}
