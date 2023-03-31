9f4e8b471566d8f1e2ff9ff5cafe61fe913a5658
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-03-31 19:14:13 +0200

    Update main.c

--- PSC_lab_6/4_2_12a/main.c
+++ PSC_lab_6/4_2_12a/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void function(int n, int *tab)
+{
+    int temp;
+    for(int i = 0; i < n/2; i++)
+    {
+        temp = *(tab + i);
+        *(tab + i) = *(tab + n - 1 - i);
+        *(tab + n - 1 - i) = temp;
+    }
+}
 int main()
 {
-    printf("Hello world!\n");
+    int n = 4;
+    int tab[] = {1,5,6,7};
+    function(n, tab);
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d\n", *(tab+i));
+    }
     return 0;
 }
+
