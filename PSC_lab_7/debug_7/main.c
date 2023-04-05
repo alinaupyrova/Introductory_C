5b3313691e5ed14cedc47b01d71f83b4cf8fac05
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-04-05 11:08:48 +0200

    Update main.c

--- PSC_lab_7/debug_7/main.c
+++ PSC_lab_7/debug_7/main.c
@@ -12,13 +12,14 @@ int main()
 {
     int tab[] = {1,2,3,4,5,6,7,8,9};
     int *wsk=tab-1;
-    int b = *(wsk+=3); //b=
-    int c = b+3; // b=  , c=
-    int d = foo(&b,&c); // b=  , c=  , d=
-    int e = (wsk+=1)[2]; // b=  , c=  , d=  , e=
-    b = (c += 8) + (e -= 3); // b=  , c=  , d=  , e=
-    e = (d *= 8) + (c /= 3); // b=  , c=  , d=  , e=
-    c = d - (b+=8); // b=  , c=  , d=  , e=
-    b = *wsk + e; // b=  , c=  , d=  , e=
+    int b = *(wsk+=3); //b=3
+    int c = b+3; // b=3  , c=6
+    int d = foo(&b,&c); // b=3  , c=6  , d=6
+    int e = (wsk+=1)[2]; // b=3 , c=6  , d=6  , e=6
+    b = (c += 8) + (e -= 3); // b=17  , c=14  , d=6  , e=3
+    e = (d *= 8) + (c /= 3); // b=17  , c=4  , d=48  , e=52
+    c = d - (b+=8); // b=25  , c=23  , d=48 , e=52
+    b = *wsk + e; // b=56  , c=23  , d=48  , e=52
     return 0;
 }
+
