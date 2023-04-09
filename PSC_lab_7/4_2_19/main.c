57302fa4de3dfb48ee6024a4e964dfdb0fa37d25
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-04-09 20:02:41 +0200

    Update main.c

--- PSC_lab_7/4_2_19/main.c
+++ PSC_lab_7/4_2_19/main.c
@@ -19,19 +19,26 @@ for(int i = 0; i <n; i++){
         tab2[j] = tab1[i];
         j++;
     }
-return tab2;
 }
-void printing(int n, int tab1[])
-{
-    for(int i=0;i<n;i++)
-    {
-        printf("%lf\n", tab1[i]);
-    }
-    printf("---\n");
+return tab2;
 }
 int main()
 {
-    int tab1[] = {1,4,5,67,8};
-    int* tab2 = copyNonZeroElemnts(5,tab1);
+    int n = 7;
+    int tab1[] = {1,4,0,5,67,8,0};
+    int* tab2 = copyNonZeroElemnts(n,tab1);
+    if (tab2 == NULL) {
+        return 1;
+    }
+
+    int non_zero_count = 0;
+    for (int i = 0; i < n; i++) {
+        if (tab1[i] != 0) {
+            non_zero_count++;
+        }
+    }
+    for (int i = 0; i < non_zero_count; i++) {
+        printf("%d ", tab2[i]);
+    }
     return 0;
 }
