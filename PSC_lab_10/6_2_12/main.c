6c2afc3ff813728eb24d4fddbb10f25df3c7787b
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-05-03 09:42:31 +0200

    Update main.c

--- PSC_lab_10/6_2_12/main.c
+++ PSC_lab_10/6_2_12/main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-int suma_elementow(int **tab,int n, int m)
-{
+
+int suma_elementow(int n, int m, int tab[n][m]) {
     int suma = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < 3; j++)
-        {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             suma += tab[i][j];
         }
     }
-    return 0;
+    return suma;
 }
-int main()
-{   int n = 2;
+
+int main() {
+    int n = 2;
     int m = 3;
-    int tab[2][3] = {{1,2,3}, {3,2,1}};
-    int result = suma_elementow(tab,n,m);
-    printf("%p\n",result);
+    int tab[2][3] = {{1, 2, 3}, {3, 2, 1}};
+    int result = suma_elementow(n, m, tab);
+    printf("%d\n", result);
     return 0;
 }
