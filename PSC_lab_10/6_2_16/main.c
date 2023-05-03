a29d540ea0a8543796b686ba4226d1443fd42926
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-05-03 18:10:06 +0200

    Update main.c

--- PSC_lab_10/6_2_16/main.c
+++ PSC_lab_10/6_2_16/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int foo( int n, int m, int tab[n][m])
+int foo( int ** tab, int n, int m)
 {
     int max_index = 0;
     double max_srednia = 0;
@@ -20,11 +20,24 @@ int foo( int n, int m, int tab[n][m])
 
 int main()
 {
-    int n = 2;
-    int m = 3;
-    int tab[2][3] = {{1, 2, 3}, {3, 2, 6}};
+ int n = 2, m = 2;
+    int ** tab= malloc(2*sizeof(int*));
+    tab[0] =  malloc(2*sizeof(int));
+    tab[1] =  malloc(2*sizeof(int));
+    tab[2] =  malloc(2*sizeof(int));
+    tab[0][0] = 3;
+    tab[0][1] = -2;
+    tab[1][0] = 1;
+    tab[1][1] = -1;
     int result = foo(tab, n, m);
-    printf("%d\n", result);
+    printf("%d", result);
+    for(int i=0;i<n;i++)
+    {
+        free(tab[i]);
+    }
+    free(tab);
+
     return 0;
 }
+
 
