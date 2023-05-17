83ade375bc0b62be946abf406f5f84a82ab80a2e
dfh <>
2023-05-17 11:18:07 +0200

    add

---
+++ PSC_lab_11/zad_1_a/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Osoba
+{
+    char imie[20];
+    int wiek;
+};
+struct Osoba initOsoba(char im[20],int wk)
+{
+   struct Osoba temp;
+   int i;
+   for(i = 0; im[i]!=0;i++)
+   {
+       temp.imie[i] = im[i];
+   }
+   temp.imie[i] = 0;
+   temp.wiek = wk;
+   return temp;
+}
+void pokazOsoba(struct Osoba arg)
+{
+    printf("Imie: %s, wiek:  %d\n",arg.imie,arg.wiek);
+}
+void urodziny(struct Osoba *wsk)
+{
+    wsk ->wiek++;
+}
+
+int main()
+{
+    struct Osoba zmienna = initOsoba("Jan",26);
+    pokazOsoba(zmienna);
+    urodziny(&zmienna);
+    pokazOsoba(zmienna);
+    return 0;
+}
