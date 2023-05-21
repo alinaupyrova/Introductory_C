f3f6b67f0888d7a10fab4a020011bf01cd7c8377
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-05-21 10:11:07 +0200

    Update main.c

--- PSC_lab_11/7_2_1/main.c
+++ PSC_lab_11/7_2_1/main.c
@@ -16,20 +16,3 @@ int main()
     printf("%f\n", obwod(tr));
     return 0;
 }
-// lub
-struct trojkat
-{
-   float a, b, c;
-};
-
-float obwod(struct trojkat arg)
-{
-    float obwod = arg.a + arg.b + arg.c;
-    return obwod;
-}
-int main()
-{
-    struct trojkat tr = {3.2, 2.3, 5.0};
-    printf("%f\n", obwod(tr));
-    return 0;
-}
