70dad779bb0c648af5e9e05ae32caad40449d01a
alinaupyrova <115549868+alinaupyrova@users.noreply.github.com>
2023-05-28 17:50:13 +0200

    Update main.c

--- PSC_lab_13/7_3_28/main.c
+++ PSC_lab_13/7_3_28/main.c
@@ -9,63 +9,70 @@ struct element
 
 int najwiekszaRoznicaZG(struct element *lista)
 {
-    struct element *wsk = lista->next;
-    int maxRoznica = abs(wsk->i - wsk->next->i);
+    struct element *wsk1 = lista->next;
+    int maxRoznica = 0;
+    int zainicjalizowana = 0;  // Zmieniona nazwa zmiennej
 
-    while (wsk->next->next != NULL)
+    while (wsk1 != NULL)
     {
-        int roznica = abs(wsk->i - wsk->next->i);
-        if (roznica > maxRoznica)
+        struct element *wsk2 = wsk1->next;
+        while (wsk2 != NULL)
         {
-            maxRoznica = roznica;
+            int roznica = abs(wsk1->i - wsk2->i);
+            if (!zainicjalizowana || roznica > maxRoznica)
+            {
+                maxRoznica = roznica;
+                zainicjalizowana = 1;
+            }
+            wsk2 = wsk2->next;
         }
-        wsk = wsk->next;
+        wsk1 = wsk1->next;
     }
 
     return maxRoznica;
 }
-
 int najwiekszaRoznicaBG(struct element *lista)
 {
     struct element *wsk = lista;
-    int maxRoznica = abs(wsk->i - wsk->next->i);
+    int maxRoznica = 0;
 
     while (wsk->next != NULL)
     {
-        int roznica = abs(wsk->i - wsk->next->i);
+        int roznica = abs(wsk->next->i - wsk->i);
         if (roznica > maxRoznica)
         {
             maxRoznica = roznica;
         }
         wsk = wsk->next;
     }
-    return maxRoznica;
 
+    return maxRoznica;
 }
+
 int main()
 {
     struct element *lista = malloc(sizeof(struct element));
     lista->next = malloc(sizeof(struct element));
-    lista->next->i = 3;
+    lista->next->i = 9;
     lista->next->next = malloc(sizeof(struct element));
     lista->next->next->i = -8;
     lista->next->next->next = malloc(sizeof(struct element));
-    lista->next->next->next->i = 10;
+    lista->next->next->next->i = 7;
     lista->next->next->next->next = malloc(sizeof(struct element));
-    lista->next->next->next->next->i = -20;
+    lista->next->next->next->next->i = -28;
     lista->next->next->next->next->next = NULL;
 
     int maxRoznicaZG = najwiekszaRoznicaZG(lista);
     printf("Najwieksza roznica dla listy z glowa wynosi  %d\n", maxRoznicaZG);
 
     struct element * lista2 = malloc(sizeof(struct element));
-    lista2->i = 3;
+    lista2->i = 5;
     lista2->next = malloc(sizeof(struct element));
-    lista2->next->i = -8;
+    lista2->next->i = 8;
     lista2->next->next = malloc(sizeof(struct element));
     lista2->next->next->i = 3;
     lista2->next->next->next = malloc(sizeof(struct element));
-    lista2->next->next->next->i = 1;
+    lista2->next->next->next->i = 2;
     lista2->next->next->next->next = NULL;
 
     int maxRoznicaBG = najwiekszaRoznicaBG(lista2);
